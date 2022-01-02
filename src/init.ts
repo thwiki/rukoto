5ebc1df8878507cdcb1818b1f7816b899fd3b6b8
Arumi <arumi@thwiki.cc>
2022-01-03 02:07:42 +0800

    Updated dependencies Fixed assertion error

--- src/init.ts
+++ src/init.ts
@@ -17,10 +17,6 @@ export async function initMediawiki() {
 
 		userAgent: `Rukoto ${version}`,
 
-		defaultParams: {
-			assert: 'user',
-		},
-
 		silent: false,
 		retryPause: 5000,
 		maxRetries: 3,
